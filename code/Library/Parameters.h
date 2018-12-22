e2b55b3be60c9d87942c9f35faa1f333632971c2
cerob <cerob@users.noreply.github.com>
2018-12-22 11:22:18 +0300

    Add new parameters

--- code/Library/Parameters.h
+++ code/Library/Parameters.h
@@ -9,13 +9,25 @@ static uint32_t TURN_LEFT_FLAG = 0;
 static uint32_t TURN_RIGHT_FLAG = 0;
 static uint32_t FORWARD_FLAG = 0;
 static uint32_t BACKWARD_FLAG = 0;
-static uint32_t ROTATION_NUMBER_FOR_90_DEGREE = 600;
+
+static const uint32_t ROTATION_NUMBER_FOR_90_DEGREE = 600;
+static const uint32_t OBSTACLE_DISTANCE = 15;
+static const uint32_t OBSTACLE_ESCAPE_DISTANCE = 30;
+
+static const char MANUEL = '*';
+static const char AUTO = '#';
+static const char WIFI_USERNAME[] = "HWLAB";
+static const char WIFI_PASSWORD[] = "12345678";
+static const char WIFI_GET_URL[] = "http://192.168.0.100:8080/HWLAB_IoT/GetInformation?ID=";
+static const char WIFI_POST_MANUEL[] = "http://192.168.0.100:8080/HWLAB_IoT/SendInformation?ID=&DATA=MANUEL";
+static const char WIFI_POST_AUTO[] = "http://192.168.0.100:8080/HWLAB_IoT/SendInformation?ID=&DATA=AUTO";
+static char START_MODE = MANUEL;
+
 
 void Set_Turn_Left_Flag(void);
 void Set_Turn_Right_Flag(void);
 void Set_Forward_Flag(void);
 void Set_Backward_Flag(void);
 void Reset_Flags(void);
-
 
 #endif
